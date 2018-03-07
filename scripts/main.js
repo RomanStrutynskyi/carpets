2e489015ca78ec4ce0bef28579318979315a01c9
Roman <strutynskyi.roman@gmail.com>
2018-03-07 10:41:53 +0200

    finish

--- scripts/main.js
+++ scripts/main.js
@@ -16,6 +16,151 @@ var Config = {
   siteName: 'Perfect application'
 };
 
+var ModalWindow = {
+  classes: {
+    root: '.js-modal-window',
+    overflow: '.js-modal-window-overflow',
+    form: '.js-modal-window-form',
+    phone: '.js-modal-window-phone',
+    text: '.js-modal-window-text',
+    send: '.js-modal-window-send',
+    iconAirplane: '.js-airplane',
+    iconCheck: '.js-check'
+  },
+  init: function init() {
+    var self = this;
+    $(this.classes.text).click(function () {
+      self.activeForm();
+    });
+    $.validator.addMethod("regex", function (value, element, regexp) {
+      if (regexp.constructor != RegExp) regexp = new RegExp(regexp);else if (regexp.global) regexp.lastIndex = 0;
+      return this.optional(element) || regexp.test(value);
+    }, "Please check your input.");
+    $(self.classes.phone).mask('+38(099)00-00-000');
+    $(self.classes.form).validate({
+      rules: {
+        phone: {
+          required: true,
+          regex: /^[+-]{1}[0-9]{2}\([0-9]{3}\)[0-9]{2}\-[0-9]{2}\-[0-9]{3}$/
+        }
+      },
+      messages: {
+        phone: {
+          number: "Неправельний формат номера ",
+          required: "Введіть номер телефону",
+          regex: "напр. +38(099)12-34-567"
+        }
+      },
+      onclick: true,
+      errorClass: "error",
+      validClass: "valid",
+      highlight: function highlight(element, errorClass, validClass) {
+        $(element).parent().children().addClass('error').removeClass(validClass);
+      },
+      unhighlight: function unhighlight(element, errorClass, validClass) {
+        $(element).parent().children().removeClass('error').addClass(validClass);
+      }
+    });
+    $(self.classes.form).submit(function (e) {
+      e.preventDefault();
+      if ($(this).valid()) {
+        $.ajax({
+          url: "/",
+          // method: $(that).attr('method'),
+          method: 'get',
+          data: $(this).serialize(),
+          contentType: "application/x-www-form-urlencoded",
+          dataType: "json",
+          success: function success() {
+            self.cleanForm();
+          }
+        }).always(function () {
+          self.cleanForm();
+        });
+      }
+    });
+  },
+  activeForm: function activeForm() {
+    $(this.classes.root).addClass('m-modal-window--active');
+  },
+  cleanForm: function cleanForm() {
+    $(this.classes.root).removeClass('m-modal-window--active').addClass('m-modal-window--send');
+    // $(this.classes.phone).fadeOut();
+    // $(this.classes.iconCheck).fadeIn();
+  }
+};
+
+var HamburgerModule = {
+  classes: {
+    root: '.js-hamburger',
+    lineTop: '.js-hamburger__line-top',
+    lineMiddle: '.js-hamburger__line-middle',
+    lineBottom: '.js-hamburger__line-bottom',
+
+    // States
+    rootActive: 'a-hamburger--active'
+  },
+  init: function init() {},
+  openHamburger: function openHamburger() {
+    $(this.classes.root).toggleClass(this.classes.rootActive);
+  },
+  closeHamburger: function closeHamburger() {}
+};
+
+var MenuModule = {
+  classes: {
+    root: '.js-menu',
+    overflow: '.m-menu__overflow',
+
+    // States
+    rootActive: 'm-menu--active',
+    overflowActive: 'm-menu__overflow--active'
+  },
+
+  openMenu: function openMenu() {
+    $(this.classes.root).toggleClass(this.classes.rootActive);
+    $(this.classes.overflow).toggleClass(this.classes.overflowActive);
+  }
+};
+
+var Header = {
+  classes: {
+    hamburder: '.js-hamburger'
+  },
+  init: function init() {
+    ModalWindow.init();
+    HamburgerModule.init();
+    $(this.classes.hamburder).on('click', function () {
+      HamburgerModule.openHamburger();
+      MenuModule.openMenu();
+    });
+  }
+};
+
+var ItemProcess = {
+  classes: {
+    root: '.js-process',
+    item: '.js-process-item'
+  },
+  tl: new TimelineLite(),
+  init: function init() {
+    if ($(window).width() < 768) {
+      this.tl.set(this.classes.item, { x: 0, y: 0, opacity: 1 });
+    } else {
+      this.tl.set(this.classes.item, { x: -50, y: -50, opacity: 0 });
+    }
+  },
+  process: function process() {
+    // console.log('hello')
+    this.tl.staggerTo(this.classes.item, .25, {
+      ease: Power4.easeInOut,
+      x: 0,
+      y: 0,
+      opacity: 1
+    }, .15);
+  }
+};
+
 var IntroSlider = {
   classes: {
     root: '.js-intro-slider'
@@ -56,6 +201,47 @@ var SectionIntro = {
   },
   arrowShow: function arrowShow() {
     // $(this.classes.prev + "," + this.classes.next).toggleClass('visible')
+  }
+};
+
+var ItemFactors = {
+  classes: {
+    root: '.js-nav-factors-slider'
+  },
+  init: function init(factorSlider) {
+    var self = this;
+    $(this.classes.root).click(function () {
+      self.onSlide(this, self, factorSlider);
+    });
+  },
+  onSlide: function onSlide(that, self, factorSlider) {
+    var changeSlide = $(that).data('nav');
+    factorSlider.slick('slickGoTo', changeSlide);
+  }
+};
+
+// import {ItemFactor} from '../../../molecules/Items/item-factor/item-factor'
+var SectionFactors = {
+  classes: {
+    slider: '.js-factors-slider',
+    sliderItem: '.js-factors-slider-item',
+    navbar: '.js-nav-factors-slider'
+  },
+  init: function init() {
+    var factorSlider = $(this.classes.slider).slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      speed: 1000,
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 3000,
+      zIndex: 2
+    });
+
+    ItemFactors.init(factorSlider);
   }
 };
 
@@ -216,7 +402,7 @@ var FormModule = {
           phone: {
             number: "Неправельний формат номера ",
             required: "Введіть номер телефону",
-            regex: "Наприклад: +38(099)12-34-567"
+            regex: "напр. +38(099)12-34-567"
           },
           description: {
             required: "Введіть повідомлення",
@@ -224,9 +410,9 @@ var FormModule = {
 
           },
           captcha_1: {
-            required: "Введіть капчу",
-            minlength: "Ваша капча закоротка",
-            maxlength: "Ваша капча задовге"
+            required: "Введіть код",
+            minlength: "Ваш код закороткий",
+            maxlength: "Ваш код задовгий"
           }
         },
         onclick: true,
@@ -292,8 +478,10 @@ console.log(Config);
 // =========================================
 // Initialization
 // =========================================
-// Module1.init();
+Header.init();
 SectionIntro.init();
+ItemProcess.init();
+SectionFactors.init();
 SectionSlider.init();
 SectionReviews.init();
 Footer.init();
@@ -301,8 +489,18 @@ Footer.init();
 $(document).ready(function () {
     $('.main').fullpage({
         fixedElements: '#header',
-        sectionSelector: 'section'
-
+        sectionSelector: 'section',
+        anchors: ['intro', 'process', 'advantages', 'factors', 'slider', 'reviews', 'footer'],
+        menu: '#menu',
+        css3: true,
+        scrollingSpeed: 700,
+        responsiveWidth: 1200,
+        responsiveHeight: 768,
+        afterLoad: function afterLoad(anchorLink, index) {
+            if (index == 2) {
+                ItemProcess.process();
+            }
+        }
     });
 });
 
